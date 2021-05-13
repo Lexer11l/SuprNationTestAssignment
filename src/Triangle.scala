58119a4a0a8d4e634aa7c68f5c745190c061eba0
kirill.meshkov <kirill.meshkov@mfms.ru>
2021-05-13 13:05:42 +0300

    working solution

--- src/Triangle.scala
+++ src/Triangle.scala
@@ -4,16 +4,38 @@ import scala.io.StdIn.readLine
 object Triangle {
 
   def main(args: Array[String]): Unit = {
-    print(readValues())
+    readValues()
+      .foldRight(List[(Int, List[Int])]())(mapLine)
+      .map(buildResult)
+      .foreach(println)
   }
 
   @tailrec
-  private def readValues(triangle: List[List[Int]] = Nil) : List[List[Int]] = {
+  private def readValues(triangle: List[List[Int]] = Nil): List[List[Int]] = {
     readLine() match {
       case "EOF" | "" => triangle
-      case line => readValues(triangle ++ List(line.split(" ").map(_.toInt).toList))
+      case line => readValues(triangle ++ parseLine(line))
     }
   }
+
+  private def parseLine(line: String) = List(line.split(" ").map(_.toInt).toList)
+
+  private def mapLine(cur: List[Int], prev: List[(Int, List[Int])]): List[(Int, List[Int])] = {
+    cur.zipWithIndex.map {
+      case (value, _) if prev.isEmpty => value -> List(value)
+      case (value, i) =>
+        val left = prev(i)
+        val right = prev(i + 1)
+        val min = if (left._1 > right._1) right else left
+        value + min._1 -> (min._2 :+ value)
+    }
+  }
+
+  def buildResult(value: (Int, List[Int])): String =
+    value match {
+      case (_, Nil) => "-1"
+      case (sum, path) => s"Minimal path is ${path.reverse.mkString(sep = " + ")} = ${sum}"
+    }
 }
 
 /*
